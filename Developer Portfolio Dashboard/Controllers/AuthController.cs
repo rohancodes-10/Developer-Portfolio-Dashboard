5c0a85eddac48247ffc9ae00342c49ec0f3b12e6
rohancodes-10 <rohankc9988@gmail.com>
2026-06-29 22:44:24 +0545

    redirecting user to github login

---
+++ Developer Portfolio Dashboard/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Developer_Portfolio_Dashboard.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IConfiguration _config;
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IActionResult Login()
+        {
+            var ClientId = _config["GitHub:ClientId"];
+            var RedirectUri = _config["GitHub:RedirectUri"];
+            var scope = "read:user repo";
+
+            var url=$"https://github.com/login/oauth/authoraize"+
+                $"?client_id={ClientId}" +
+                $"&redirect_uri={RedirectUri}" +
+                $"&Scope={scope}";
+            return Redirect(url);
+        }
+    }
+}
