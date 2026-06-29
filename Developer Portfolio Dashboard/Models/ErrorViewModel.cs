ef0fd86509d9f90a8dd26f735bcd20a7ed15a583
rohancodes-10 <rohankc9988@gmail.com>
2026-06-29 22:24:42 +0545

    Add project files.

---
+++ Developer Portfolio Dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Developer_Portfolio_Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
